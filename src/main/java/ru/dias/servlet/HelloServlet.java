097a897b2c753511910ce482c20ec42a9bafbf09
Dmitrenko <Dias64@mail.ru>
2020-08-03 10:42:04 +0300

    homework6

---
+++ src/main/java/ru/dias/servlet/HelloServlet.java
@@ -0,0 +1,17 @@
+package ru.dias.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@WebServlet(urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+//        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+}
